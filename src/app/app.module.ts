91f7007cadbe66cecc8e00e1f6b828c8245ce0ab
Andy Alekhin <andy.alekhin@gmail.com>
2017-09-15 20:53:08 +0300

    added avatar and contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AvatarComponent } from '../components/avatar/avatar.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { ContactsComponent } from '../components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AvatarComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/components/avatar/avatar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css']
+})
+
+export class AvatarComponent {}
