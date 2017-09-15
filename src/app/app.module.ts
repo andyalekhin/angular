44ab2038f4ffedf81efa05862f64e55f85915e2a
Andy Alekhin <andy.alekhin@gmail.com>
2017-09-15 23:43:50 +0300

    added fontawesome, education, work and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { AvatarComponent } from '../components/avatar/avatar.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { ContactsComponent } from '../components/contacts/contacts.component';
+import { WorkComponent } from '../components/work/work.component';
+import { EducationComponent } from '../components/education/education.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AvatarComponent,
-    ContactsComponent
+    ContactsComponent,
+    EducationComponent,
+    WorkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
